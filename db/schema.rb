5f5f52ecb12fc4df7832f6da4881a2f86da71649
sakitayu <sakitakitakita@gmail.com>
2020-06-26 15:26:49 +0900

    Messageモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_061722) do
+ActiveRecord::Schema.define(version: 2020_06_26_062628) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
